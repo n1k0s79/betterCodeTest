d01cdad14f8a45d5c00b0d1f2248da2cdc47b2cd
Nikos Sandalidis <nikos@ti-soft.com>
2021-09-23 16:48:55 +0300

    Add tests

---
+++ Physics.Test/Instantiations.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Physics.Test
+{
+    [TestClass]
+    public class Instantiations
+    {
+        [TestMethod]
+        public void Instantiate()
+        {
+            var s = new Sun();
+        }
+    }
+}
---
+++ Plumbing.Tests/Instantiations.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Plumbing.Tests
+{
+    [TestClass]
+    public class Instantiations
+    {
+        [TestMethod]
+        public void Instantiate()
+        {
+            var k = new Pipe();
+        }
+    }
+}
