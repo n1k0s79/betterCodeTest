586974e8a868915d97b7f88753db5e84845a2d63
Nikos Sandalidis <nikos@ti-soft.com>
2021-09-24 11:34:08 +0300

    Test code quality

--- Physics/Sun.cs
+++ Physics/Sun.cs
@@ -7,10 +7,10 @@ namespace Physics
         public double GetInclinationAngle()
         {
             var p1 = new Geometry.Point(1, 2);
-            //var p2 = new Geometry.Point(3, 4);
-            //var dist = p1.GetDistance(p2);
-            //return dist;
-            return p1.X;
+            var p2 = new Geometry.Point(3, 4);
+            var dist = p1.GetDistance(p2);
+            return dist;
+            //return p1.X;
         }
     }
 }
