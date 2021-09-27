4d8584a9d75ecbc97863fc670f0bb81d0d2213a7
Nikos Sandalidis <nikos@ti-soft.com>
2021-09-27 09:27:46 +0300

    Add test code

--- Architectural/Window.cs
+++ Architectural/Window.cs
@@ -13,5 +13,10 @@ namespace Architectural
             this.area = area;
             var inclination = new Physics.Sun().GetInclinationAngle();            
         }
+
+        public double GetArea(int a1, int a2, int a3, int a4, int a5, int a6, int a7, int a8, int a9, int a10)
+        {
+            return 1.1;
+        }
     }
 }
